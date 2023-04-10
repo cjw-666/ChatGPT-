24bb86e2a39568e61c6173e085f2dd6fc4874bb6
JW CHEN <39663688+cjw-666@users.noreply.github.com>
2023-04-11 02:56:09 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,7 +16,7 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://www.xstech.top/wp-content/uploads/2023/03/128x128.png',
-      name: '潇洒科技',
+      name: '潇洒科技ChatGPT 本地化版本',
       description: '官网链接- <a href="http://www.xstech.top/" class="text-blue-500" target="_blank" >XsTech</a>',
     },
   }
